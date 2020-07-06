4eb06f0b4f59b4b026fb0fee71e9f08cf1475150
liliang <besruiven@gmail.com>
2020-07-06 17:32:19 +0800

    项目初始化

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="login_container">
+        <h1>login</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login"
+}
+</script>
+
+<style scoped>
+   
+</style>
